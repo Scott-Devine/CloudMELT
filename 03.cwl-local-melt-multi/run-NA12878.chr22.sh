ce3dc7e56c192d592540493bba6c117f24b8e571
Jonathan Crabtree <jonathancrabtree@gmail.com>
2019-02-14 09:42:51 -0500

    Remove -test suffix from yml files.

--- 03.cwl-local-melt-multi/run-NA12878.chr22.sh
+++ 03.cwl-local-melt-multi/run-NA12878.chr22.sh
@@ -5,21 +5,15 @@ export RUNNER='toil-cwl-runner --retryCount 0'
 # verbose logging
 #$RUNNER --logDebug melt.cwl melt.yml
 
-# Preprocess
-#$RUNNER melt-pre.cwl melt-pre-test.yml 
-
-# IndivAnalyis
-#$RUNNER melt-ind.cwl melt-ind-test.yml 
-
 # WF1. Preprocess - IndivAnalysis
 # TODO - add Coverage
-$RUNNER melt-split-pre-ind-cov.cwl melt-pre-ind-cov-test.yml
+$RUNNER melt-split-pre-ind-cov.cwl NA12878.chr22/melt-pre-ind-cov.yml
 
 # WF2. GroupAnalysis
-$RUNNER melt-grp.cwl melt-grp-test.yml 
+$RUNNER melt-grp.cwl NA12878.chr22/melt-grp.yml 
 
 # WF3. Genotyping
-$RUNNER --retryCount 0 melt-gen.cwl melt-gen-test.yml 
+$RUNNER --retryCount 0 melt-gen.cwl NA12878.chr22/melt-gen.yml 
 
 # WF4. MakeVCF
-$RUNNER melt-vcf.cwl melt-vcf-test.yml 
+$RUNNER melt-vcf.cwl NA12878.chr22/melt-vcf.yml 
--- 03.cwl-local-melt-multi/run-NA12878.sh
+++ 03.cwl-local-melt-multi/run-NA12878.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
-# test Preprocess subflow
-#toil-cwl-runner melt-pre.cwl melt-pre-test.yml 
-
-# test IndivAnalyis subflow
-#toil-cwl-runner melt-ind.cwl melt-ind-test.yml 
-
-# test workflow wrapping a command
-toil-cwl-runner melt-split-pre-ind-cov.cwl melt-wf-test.yml
+export RUNNER='toil-cwl-runner --retryCount 0'
 
 # verbose logging
-#toil-cwl-runner --logDebug melt.cwl melt.yml
+#$RUNNER --logDebug melt.cwl NA12878/melt.yml
+
+# Preprocess
+#$RUNNER melt-pre.cwl NA12878/melt-pre.yml 
+
+# IndivAnalyis
+#$RUNNER melt-ind.cwl NA12878/melt-ind.yml 
+
+# WF1. Preprocess - IndivAnalysis
+# TODO - add Coverage
+$RUNNER melt-split-pre-ind-cov.cwl NA12878/melt-pre-ind-cov.yml
+
+# WF2. GroupAnalysis
+$RUNNER melt-grp.cwl NA12878/melt-grp.yml 
+
+# WF3. Genotyping
+$RUNNER --retryCount 0 melt-gen.cwl NA12878/melt-gen.yml 
+
+# WF4. MakeVCF
+$RUNNER melt-vcf.cwl NA12878/melt-vcf.yml
