7c8d841043a3c7bd355eb8b50e56fbe2d36e1ffc
Jonathan Crabtree <jonathancrabtree@gmail.com>
2019-03-08 10:00:18 -0500

    Add filtered AluY .bed file.

--- 06.cwl-melt-docker/docker/build-docker.sh
+++ 06.cwl-melt-docker/docker/build-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #cp ../../tarballs/MELTv2.1.5.tar.gz ./
-#cp ../../05.cwl-local-multi-sample-multi-ME/AluY.deletion.filtered.bed ./
-#cp ../../HERVK_MELT.zip ./
+#cp ../../add_bed_files/1KGP_Hg19/05.cwl-local-multi-sample-multi-ME/AluY.deletion.filtered.bed ./
+#cp ../../me_refs/1KGP_HG19/HERVK_MELT.zip ./
 docker build -t umigs/cloud-melt-v1.0.0 .
