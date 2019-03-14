972c93c465596d4fd18bf9d1454ee88e22b7c7b2
Jonathan Crabtree <jonathancrabtree@gmail.com>
2019-03-14 12:48:20 -0400

    Add test cases.

--- 07.cwl-melt-aws/run-AWS-NA12878.chr22-test.sh
+++ 07.cwl-melt-aws/run-AWS-NA12878.chr22-test.sh
@@ -8,12 +8,12 @@ export RUNNER='toil-cwl-runner --retryCount 0'
 # melt-del-multi.cwl
 # --------------------------------------------
 
-$RUNNER --clean never \
- --jobStore aws:us-east-1:toil-tjs1 \
- --logLevel DEBUG \
- --logFile melt-del-multi.log \
- --batchSystem mesos \
- melt-del-multi.cwl config/melt-del-multi-NA12878.chr22.yml
+#$RUNNER --clean never \
+# --jobStore aws:us-east-1:toil-tjs1 \
+# --logLevel DEBUG \
+# --logFile melt-del-multi.log \
+# --batchSystem mesos \
+# melt-del-multi.cwl config/melt-del-multi-NA12878.chr22.yml
 
 # manually clean aws job store
 #toil clean aws:us-east-1:toil-tjs1
@@ -21,7 +21,20 @@ $RUNNER --clean never \
 # for debugging: --cleanWorkDir never 
 
 # --------------------------------------------
-# melt-pre.cwl
+# melt-split
 # --------------------------------------------
 
-# TODO
+#$RUNNER --clean never \
+# --jobStore aws:us-east-1:toil-tjs1 \
+# --logLevel DEBUG \
+# --logFile melt-split-multi.log \
+# --batchSystem mesos \
+# melt-split-multi-mosdepth-cov.cwl config/melt-split-multi-NA12878.chr22.yml
+
+$RUNNER --clean never \
+ --jobStore aws:us-east-1:toil-tjs1 \
+ --logLevel DEBUG \
+ --logFile melt-split-multi.log \
+ --batchSystem mesos \
+ melt-split-multi-mosdepth-cov.cwl config/melt-split-multi-NA12878.chr22.chrX.all.yml
+
