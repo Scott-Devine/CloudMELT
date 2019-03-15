7101a19343eee88f86f3f3de7294a16b968fcfb8
Jonathan Crabtree <jonathancrabtree@gmail.com>
2019-03-14 21:22:36 -0400

    Add test script for whole genome run.

---
+++ 07.cwl-melt-aws/run-AWS-NA12878-test.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Test MELT pipeline steps on AWS/Toil
+
+export RUNNER='toil-cwl-runner --retryCount 0'
+
+# --------------------------------------------
+# melt-del-multi.cwl
+# --------------------------------------------
+
+#$RUNNER --clean never \
+# --jobStore aws:us-east-1:toil-tjs1 \
+# --logLevel DEBUG \
+# --logFile melt-del-multi.log \
+# --batchSystem mesos \
+# melt-del-multi.cwl config/melt-del-multi-NA12878.chr22.yml
+
+# manually clean aws job store
+#toil clean aws:us-east-1:toil-tjs1
+
+# for debugging: --cleanWorkDir never 
+
+# --------------------------------------------
+# melt-split
+# --------------------------------------------
+
+# DEBUG
+
+$RUNNER --clean never \
+ --jobStore aws:us-east-1:toil-tjs1 \
+ --logLevel DEBUG \
+ --logFile melt-split-multi.log \
+ --batchSystem mesos \
+ melt-split-multi-mosdepth-cov.cwl config/melt-split-multi-NA12878.chr22.yml
+
+#$RUNNER \
+# --jobStore aws:us-east-1:toil-tjs1 \
+# --logLevel DEBUG \
+# --logFile melt-split-multi.log \
+# --batchSystem mesos \
+# melt-split-multi-mosdepth-cov.cwl config/melt-split-multi-NA12878.LINE1.yml
+
