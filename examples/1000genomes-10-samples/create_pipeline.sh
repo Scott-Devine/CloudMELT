46794382c891174406bcd67f1f05c3359babd619
Jonathan Crabtree <jonathancrabtree@gmail.com>
2019-04-24 14:44:49 -0400

    Use env var for software install location.

--- examples/1000genomes-10-samples/create_pipeline.sh
+++ examples/1000genomes-10-samples/create_pipeline.sh
@@ -5,7 +5,7 @@ export CLOUD_MELT_HOME=~/MELT
 
 # create pipeline for 10 low-coverage 1000 Genomes samples
 #  -edit toil_jobstore to reflect the desired AWS zone
-../../bin/create_pipeline.pl --sample_uri_list=sample_uris.txt \
+$CLOUD_MELT_HOME/bin/create_pipeline.pl --sample_uri_list=sample_uris.txt \
  --config_dir=./config.in \
  --workflow_dir=./melt-workflow \
  --toil_jobstore='aws:us-east-1:tj1'
