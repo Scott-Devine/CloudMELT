d669f8eefdd15c01def991f025d9507bff32b84e
Jonathan Crabtree <jonathancrabtree@gmail.com>
2019-04-24 15:13:59 -0400

    Separate hg19 and hg38 Docker images.

---
+++ docker/hg19/push-to-aws-ecr.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#(aws ecr get-login --no-include-email --region us-east-1)
+#  -> returns docker login command to run
+
+docker tag umigs/cloud-melt-hg19-v1.0.0:latest 205226202704.dkr.ecr.us-east-1.amazonaws.com/umigs/melt:hg19-latest
+docker push 205226202704.dkr.ecr.us-east-1.amazonaws.com/umigs/melt:hg19-latest
+
