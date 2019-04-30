777dbd2356961fe466d16db286a301bd546f0433
Jonathan Crabtree <jonathancrabtree@gmail.com>
2019-04-30 09:44:29 -0400

    Amend comment.

--- examples/1000genomes-10-samples-high-hg38/create_pipeline.sh
+++ examples/1000genomes-10-samples-high-hg38/create_pipeline.sh
@@ -3,12 +3,12 @@
 # set to location of CloudMELT source code
 export CLOUD_MELT_HOME=~/MELT
 
-# create pipeline for 10 low-coverage 1000 Genomes samples
+# create pipeline for 10 high-coverage 1000 Genomes samples
 #  -edit toil_jobstore to reflect the desired AWS zone
 $CLOUD_MELT_HOME/bin/create_pipeline.pl --sample_uri_list=sample_uris.txt \
  --config_dir=./config.in \
  --workflow_dir=./melt-workflow \
  --docker_image_uri='205226202704.dkr.ecr.us-east-1.amazonaws.com/umigs/melt:hg38-latest' \
  --sample_regex='([^/]+)\.final\.cram' \
- --coverage_method=user \
+ --coverage_method=user \p
  --toil_jobstore='aws:us-east-1:tj1'
