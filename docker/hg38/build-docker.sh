2402ee558222c4205f254cb4e00a454c0d147a55
Jonathan Crabtree <jonathancrabtree@gmail.com>
2019-05-11 08:58:15 -0400

    Add r38-based jar file with queryMate() patch to hg38 Docker image.

--- docker/hg38/build-docker.sh
+++ docker/hg38/build-docker.sh
@@ -6,5 +6,6 @@ cp ../mosdepth2cov.py ./
 cp ../bowtie2-2.3.4.3-linux-x86_64.zip ./
 cp ../cwltool_then_clean_tmp ./
 cp ../get_bam_and_bai.pl ./
+cp ../../MELT-r38-v4-jc.jar ./
 docker build -t umigs/cloud-melt-hg38-v1.0.0 .
-rm GRCh38* commas_to_newlines.sh mosdepth2cov.py MELTv2.1.5.tar.gz bowtie2-2.3.4.3-linux-x86_64.zip cwltool_then_clean_tmp get_bam_and_bai.pl
+rm GRCh38* commas_to_newlines.sh mosdepth2cov.py MELTv2.1.5.tar.gz bowtie2-2.3.4.3-linux-x86_64.zip cwltool_then_clean_tmp get_bam_and_bai.pl MELT-r38-v4-jc.jar 
