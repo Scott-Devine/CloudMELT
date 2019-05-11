5fe5f89a478205f2f89ab60eaf9e48e306f7dc5a
Jonathan Crabtree <jonathancrabtree@gmail.com>
2019-05-11 09:08:20 -0400

    Add new low-coverage example with 2 samples and --run_melt_deletion option.

---
+++ examples/1000genomes-2-samples-split-del-hg19/create_pipeline.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# set to location of CloudMELT source code
+export CLOUD_MELT_HOME=~/MELT
+
+# create pipeline for 2 low-coverage 1000 Genomes samples
+#  -edit toil_jobstore to reflect the desired AWS zone
+$CLOUD_MELT_HOME/bin/create_pipeline.pl --sample_uri_list=sample_uris.txt \
+ --config_dir=./config.in \
+ --workflow_dir=./melt-workflow-del \
+ --docker_image_uri='205226202704.dkr.ecr.us-east-1.amazonaws.com/umigs/melt:hg19-latest' \
+ --coverage_method=user \
+ --run_melt_deletion \
+ --toil_jobstore='aws:us-east-1:jc-tj2'
