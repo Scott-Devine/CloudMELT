396fd4b8aac9e5bc8bc8614f054a8699a2b84ee1
Jonathan Crabtree <jonathancrabtree@gmail.com>
2019-05-24 15:01:26 -0400

    Add 10 and 2 sample URI files.

--- examples/1000genomes-10-samples-high-hg38/create_pipeline.sh
+++ examples/1000genomes-10-samples-high-hg38/create_pipeline.sh
@@ -5,7 +5,7 @@ export CLOUD_MELT_HOME=~/MELT
 
 # create pipeline for 10 high-coverage 1000 Genomes samples
 #  -edit toil_jobstore to reflect the desired AWS zone
-$CLOUD_MELT_HOME/bin/create_pipeline.pl --sample_uri_list=sample_uris.txt \
+$CLOUD_MELT_HOME/bin/create_pipeline.pl --sample_uri_list=10_sample_uris.txt \
  --config_dir=./config.in \
  --workflow_dir=./melt-workflow \
  --docker_image_uri='205226202704.dkr.ecr.us-east-1.amazonaws.com/umigs/melt:hg38-latest' \
