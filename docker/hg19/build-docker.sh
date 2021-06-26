7163db03abd12064fd9cc7a6af0b613bd8f14534
Jonathan Crabtree <jonathancrabtree@gmail.com>
2021-06-25 21:14:13 -0400

    Increment container version to 1.0.1

--- docker/hg19/build-docker.sh
+++ docker/hg19/build-docker.sh
@@ -9,6 +9,5 @@ cp ../bowtie2-2.3.4.3-linux-x86_64.zip ./
 cp ../cwltool_then_clean_tmp ./
 cp ../get_bam_and_bai.pl ./
 cp ../upload_file.pl ./
-cp ../MELT-r38-v4-jc.jar ./
-docker build -t umigs/cloud-melt-hg19-v1.0.0 .
-rm hs37d5.fa* commas_to_newlines.sh mosdepth2cov.py MELTv2.1.5fast.tar.gz AluY.deletion.filtered.bed HERVK_MELT.zip bowtie2-2.3.4.3-linux-x86_64.zip cwltool_then_clean_tmp get_bam_and_bai.pl upload_file.pl MELT-r38-v4-jc.jar 
+docker build -t umigs/cloud-melt-hg19-v1.0.1 .
+rm hs37d5.fa* commas_to_newlines.sh mosdepth2cov.py MELTv2.1.5fast.tar.gz AluY.deletion.filtered.bed HERVK_MELT.zip bowtie2-2.3.4.3-linux-x86_64.zip cwltool_then_clean_tmp get_bam_and_bai.pl upload_file.pl
